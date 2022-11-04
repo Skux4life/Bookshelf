f041641568f4eebb7a17e4db7404ce3078a641fe
Matt Ellis <mattellis1234@gmail.com>
2022-11-04 19:24:53 +1100

    Initial commit

---
+++ app/src/main/java/com/example/bookshelf/HomeScreen.kt
@@ -0,0 +1,64 @@
+@file:OptIn(ExperimentalFoundationApi::class)
+
+package com.example.bookshelf
+
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.example.bookshelf.model.Book
+
+@Composable
+fun HomeScreen(
+    modifier: Modifier = Modifier
+) {
+
+}
+
+@Composable
+fun LoadingScreen(modifier: Modifier = Modifier) {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = modifier.fillMaxSize()
+    ) {
+        Image(
+            modifier = Modifier.size(200.dp),
+            painter = painterResource(id = R.drawable.loading_img),
+            contentDescription = stringResource(R.string.loading)
+        )
+    }
+}
+
+@Composable
+fun ErrorScreen(modifier: Modifier = Modifier) {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = modifier.fillMaxSize()
+    ) {
+        Text(stringResource(R.string.error))
+    }
+}
+
+@Composable
+fun BooksGridScreen(
+    books: List<Book>,
+    modifier: Modifier = Modifier
+) {
+    LazyVerticalGrid(
+        columns = GridCells.Adaptive(150.dp),
+        modifier = modifier,
+        contentPadding = PaddingValues(4.dp)
+    ) {
+    }
+}
